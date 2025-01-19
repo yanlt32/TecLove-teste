9e544f6bb8045fa74283aba4078e8d67b35a6cb5
yan <ladeiatortelli8@outlook.com>
2025-01-19 12:08:36 -0300

    Teclove-teste

---
+++ script.js
@@ -0,0 +1,9 @@
+function openDemo(demoId) {
+    const demoModal = document.getElementById(demoId);
+    demoModal.style.display = "flex";
+}
+
+function closeDemo(demoId) {
+    const demoModal = document.getElementById(demoId);
+    demoModal.style.display = "none";
+}
